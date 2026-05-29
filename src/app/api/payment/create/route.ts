0bd71c4b80393b2c005c9ab418d6a5268d435547
Shobun Kollied Anwar <shobunkollieda@gmail.com>
2026-05-29 14:17:25 +0700

    add clientKey

--- src/app/api/payment/create/route.ts
+++ src/app/api/payment/create/route.ts
@@ -5,6 +5,7 @@ import midtransClient from 'midtrans-client'
 const snap = new midtransClient.Snap({
     isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
     serverKey: process.env.MIDTRANS_SERVER_KEY!,
+    clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY!,
 })
 
 export async function POST(request: NextRequest) {
